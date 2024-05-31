37248a122b76e844be814be7f3b2ef92480ee970
levaniko77 <168901442+levaniko77@users.noreply.github.com>
2024-05-31 11:44:39 -0700

    goa

---
+++ level 3/homework/classwork/classwork.py
@@ -0,0 +1,65 @@
+
+name = 'Levani'
+
+last_name = 'sadghobelashvili'
+
+age = '12'
+
+hobby = 'programming'
+
+country = 'georgia'
+
+print(name)
+print(last_name)
+print(age)
+print(hobby)
+print(country)
+
+
+print(31 + 74)
+print(11 + 32)
+print(364 + 768)
+print(415 + 625)
+print(798 + 1091)
+print(2025 + 4358)
+print(16008 + 1702)
+print(12438 + 26081)
+print(30019 + 5567)
+print(51748 + 78901)
+print(92016 + 87255)
+
+print(58 - 35)
+print(108 - 77)
+print(254 - 175)
+print(706 - 327)
+print(1208 - 208)
+print(3048 - 1765)
+print(4518 - 2918)
+print(8432 - 5768)
+print(8510 - 6099)
+print(12800 - 10796)
+print(17363 - 14618)
+
+print(58 * 27)
+print(275 * 511)
+print(318 * 363)
+print(518 * 817)
+print(547 * 409)
+print(680 * 830)
+print(908 * 691)
+print(1561 * 1331)
+print(1708 * 1488)
+print(2381 * 402)
+print(2424 * 1007)
+
+print(4536 // 21)
+print(38976 // 64)
+print(18175 // 25)
+print(5775 // 15)
+print(20096 // 32)
+print(4158 // 21)
+print(4944 // 24)
+print(4564 // 28)
+print(3696 // 42)
+print(11472 // 16)
+print(22680 // 28)
