667ca85348f84abf49e954b45ce64251959a3a4d
levaniko77 <168901442+levaniko77@users.noreply.github.com>
2024-07-31 04:08:59 -0700

    goa

---
+++ level 5/homework.py
@@ -0,0 +1,24 @@
+name = "levani"
+
+
+last_name = "sadghobelashvili"
+
+
+age = "13"
+
+
+favorite_food = "potato"
+
+
+favorite_color = "purple"
+
+
+favorite_car = "toyota prius"
+
+
+print(name)
+print(last_name)
+print(age)
+print(favorite_food)
+print(favorite_color)
+print(favorite_car)
